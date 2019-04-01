afe677e7c9ce25da29e28d4a165056d0dd8928d8
Josmer Delgado <jjdel2005@gmail.com>
2019-04-01 18:52:17 -0300

    delete not needed key

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Firebase, { FirebaseContext } from "./components/Firebase";
 
 ReactDOM.render(
   <FirebaseContext.Provider value={new Firebase()}>
-    <App key="main" />
+    <App />
   </FirebaseContext.Provider>,
   document.getElementById("root")
 );
